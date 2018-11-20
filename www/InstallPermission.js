bf831bfc542d30bce4fd464f855583bf392cc2b7
nobage <nobage@163.com>
2018-11-20 12:55:22 +0800

    modify

--- www/InstallPermission.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.checkInstallPermission = function (success, error) {
-    exec(success, error, 'InstallPermission', 'checkInstallPermission', []);
-};
-exports.switchToAppInstallSettings = function () {
-    exec(null, null, 'InstallPermission', 'switchToAppInstallSettings', []);
-};
